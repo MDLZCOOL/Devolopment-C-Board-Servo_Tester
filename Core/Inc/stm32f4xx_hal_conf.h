c1aedc5947db83b3cc6076adf0c6c0147f3c1c80
mdlzcool <mdlzcool@foxmail.com>
2024-10-21 19:17:07 +0800

    feat(adc): 添加 ADC1 和 ADC3 的初始化代码
    - 新增 adc.c 和 adc.h 文件，实现 ADC1 和 ADC3 的初始化
    - 更新 .mxproject 文件，添加新的 ADC 相关配置

--- Core/Inc/stm32f4xx_hal_conf.h
+++ Core/Inc/stm32f4xx_hal_conf.h
@@ -38,7 +38,7 @@
 #define HAL_MODULE_ENABLED
 
   /* #define HAL_CRYP_MODULE_ENABLED */
-/* #define HAL_ADC_MODULE_ENABLED */
+#define HAL_ADC_MODULE_ENABLED
 /* #define HAL_CAN_MODULE_ENABLED */
 /* #define HAL_CRC_MODULE_ENABLED */
 /* #define HAL_CAN_LEGACY_MODULE_ENABLED */
@@ -214,7 +214,7 @@
 #define MAC_ADDR5   0U
 
 /* Definition of the Ethernet driver buffers size and count */
-#define ETH_RX_BUF_SIZE                 /* buffer size for receive               */
+#define ETH_RX_BUF_SIZE                ETH_MAX_PACKET_SIZE /* buffer size for receive               */
 #define ETH_TX_BUF_SIZE                ETH_MAX_PACKET_SIZE /* buffer size for transmit              */
 #define ETH_RXBUFNB                    4U       /* 4 Rx buffers of size ETH_RX_BUF_SIZE  */
 #define ETH_TXBUFNB                    4U       /* 4 Tx buffers of size ETH_TX_BUF_SIZE  */
